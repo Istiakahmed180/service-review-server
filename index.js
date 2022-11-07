22014135c393965f233e4f4f47fe70caa72026b6
Istiakahmed180 <Istiakahmed180@gmail.com>
2022-11-08 01:43:06 +0600

    Services Data Created Successfully

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const services = require("./services.json");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware code
 app.use(cors());
 app.use(express.json());
+
+// mongodb client
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pwqsejd.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 // testing server
 app.get("/", (req, res) => {
@@ -14,5 +26,10 @@ app.get("/", (req, res) => {
 
 // listen server
 app.listen(port, () => {
+  client.connect((err) => {
+    if (err) {
+      console.log(err.message);
+    }
+  });
   console.log(`Assignment Project Running On Port:${port}`);
 });
