285371b17adb52c4f496087003eda43b99cef372
Istiakahmed180 <Istiakahmed180@gmail.com>
2022-11-10 11:15:43 +0600

    jwt token client side emplement successfully

--- index.js
+++ index.js
@@ -19,6 +19,21 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     // service collection for mongodb database
@@ -63,7 +78,12 @@ async function run() {
     });
 
     // reviews all data get
-    app.get("/reviews", async (req, res) => {
+    app.get("/reviews", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      console.log(decoded);
+      if (decoded.email !== req.query.email) {
+        res.status(403).send({ message: "Unauthorized Access" });
+      }
       let query = {};
       if (req.query.email) {
         query = {
